127e539aa126da8aa955515550778c837482657a
Tanay <tanaydpatel@gmail.com>
2021-07-03 00:37:00 +0530

    🧹 initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./css/styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="heading">Welcome to Find your bank app.</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+// Create a theme instance.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#00d09c",
+    },
+    secondary: {
+      main: "#5367ff",
+    },
+  },
+});
+
+export default theme;
