d75c2a124029f03978ea8e1753c6e8332843c5f8
Tanay <tanaydpatel@gmail.com>
2021-07-03 19:22:20 +0530

    ✅ Added redux

--- src/App.jsx
+++ src/App.jsx
@@ -10,34 +10,38 @@ import "./css/styles.css";
 import AllBanks from "./pages/AllBanks";
 import BankDetails from "./pages/BankDetails";
 import Favorites from "./pages/Favorites";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <main>
-          <Switch>
-            <Route path="/all-banks">
-              <AllBanks />
-            </Route>
-            <Route path="/bank-details/:id">
-              <BankDetails />
-            </Route>
-            <Route path="/favorites">
-              <Favorites />
-            </Route>
-            <Route
-              exact
-              path="/"
-              render={() => {
-                return <Redirect to="/all-banks" />;
-              }}
-            />
-          </Switch>
-        </main>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <main>
+            <Switch>
+              <Route path="/all-banks">
+                <AllBanks />
+              </Route>
+              <Route path="/bank-details/:id">
+                <BankDetails />
+              </Route>
+              <Route path="/favorites">
+                <Favorites />
+              </Route>
+              <Route
+                exact
+                path="/"
+                render={() => {
+                  return <Redirect to="/all-banks" />;
+                }}
+              />
+            </Switch>
+          </main>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
